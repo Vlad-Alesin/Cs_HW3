3fd22ad25c2fd83f9a970cf9e90347ffdfd174f1
Vlad A <seralesin@mail.ru>
2022-12-25 00:40:46 +0300

    сделал T21

---
+++ T21/T21.cs
@@ -0,0 +1,30 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int x1=GetNumber("введите координату Х первой точки");
+int y1=GetNumber("введите координату У первой точки");
+int z1=GetNumber("введите координату z первой точки");
+int x2=GetNumber("введите координату Х второй точки");
+int y2=GetNumber("введите координату У второй точки");
+int z2=GetNumber("введите координату z первой точки");
+
+double rez = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+Console.WriteLine(Math.Round(rez,2));
