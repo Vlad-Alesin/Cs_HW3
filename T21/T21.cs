5f6d671e7ae0c5980132aff4928d12c4a1c0ddda
Vlad A <seralesin@mail.ru>
2022-12-25 00:46:55 +0300

    исправлено сообщение при запросе координаты z2

--- T21/T21.cs
+++ T21/T21.cs
@@ -24,7 +24,7 @@ int y1=GetNumber("введите координату У первой точки
 int z1=GetNumber("введите координату z первой точки");
 int x2=GetNumber("введите координату Х второй точки");
 int y2=GetNumber("введите координату У второй точки");
-int z2=GetNumber("введите координату z первой точки");
+int z2=GetNumber("введите координату z второй точки");
 
 double rez = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
 Console.WriteLine(Math.Round(rez,2));
