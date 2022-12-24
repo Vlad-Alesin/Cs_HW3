a2ff427ac4dfe83876dad07a0817aa9e60565517
Vlad A <seralesin@mail.ru>
2022-12-25 00:42:43 +0300

    сделал T23

---
+++ T23/T23.cs
@@ -0,0 +1,31 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int n=GetNumber("введите число");
+int i=1;
+
+Console.WriteLine("полученный результат");
+while (i<=n)
+{
+    Console.WriteLine(Math.Pow(i,3));
+    i++;
+}
+
